b0f07601cdffaac478d9112261142a1a0f9910f8
FSVieira21286 <aluno21286@ipt.pt>
2021-04-26 15:05:19 +0100

    Adicionar arquivos de projeto.

---
+++ ProfFinalTI2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProfFinalTI2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
